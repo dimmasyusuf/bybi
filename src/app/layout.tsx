56a4f0f97724d06b7b76fea0dd6868b20f11276c
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2025-02-08 09:05:30 +0700

    chore: setup react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
-import type { Metadata } from "next";
-import Fonts from "@/assets/fonts";
-import "@/styles/globals.css";
+import type { Metadata } from 'next';
+import Fonts from '@/assets/fonts';
+import Providers from '@/providers';
+import { cn } from '@/lib/utils';
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: "Bybi",
-  description: "A Personal Blog by Habibi Zaidatul Mamuriyah",
+  title: 'Bybi',
+  description: 'A Personal Blog by Habibi Zaidatul Mamuriyah',
 };
 
 export default function RootLayout({
@@ -14,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${Fonts} font-sans antialiased`}>{children}</body>
+      <body className={cn('font-sans antialiased', Fonts)}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
